c442878df8e404fbf2b33f167069ccaf0c33983f
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-01-22 15:56:04 +0000

    more changes

--- react-app/src/components/Activities.jsx
+++ react-app/src/components/Activities.jsx
@@ -4,9 +4,11 @@ import {GetActivitiesAA, GetActivitiesPuttoScreen} from "./GetDataAA";
 
 export const Activities = () => {
     return(
-        <div className='activities-container'>
+        <div className="activities-container">
+        <div className='activities-grid-container'>
             <GetData/>
             <GetActivitiesAA/>
             <GetActivitiesPuttoScreen/>
-        </div>);
+        </div></div>
+    );
 }
